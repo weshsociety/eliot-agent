b80a0a69203d156b2d1ae28701909f167fba6c80
WeshSociety <contact@weshsociety.org>
2026-07-05 16:25:57 +0000

    Revert "🔥 MONNAIE.EXE IPFS LIVE: line"
    This reverts commit 912c2b91e03dedbbc36d39dc52f2ff528b7e4b79.

--- fix_synthesize.sh
+++
@@ -1,1 +0,0 @@
-sed -i '/case "\$command" in/,/"investigate"|"inv")/s/"investigate"|"inv")/"synthesize"|"syn"|"think")\n            mr_synthesize\n            ;;\n        "investigate"|"inv")/' BUREAU_42_mr_robot.sh
